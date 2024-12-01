e88aba0fc58f105d6567d854d96f45bd60525e7c
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2024-12-01 12:08:16 -0300

    Atualizacao do jogo, implementando desenho do hangman

--- 04_game/jogoDaForca.py
+++ 04_game/jogoDaForca.py
@@ -1,63 +1,145 @@
-# Projeto Jogo Da Forca
+# Hangman Game - Code with Variables in English
 
-#random escolhe aleatoriamente a palavra usada na forca
+# Importing libraries
 import random
-from os import system, name;
+from os import system, name
 
-#Funcao para limpar a tela a cada rodada
-def cleanScreen():
-
-    #windows
+# Function to clear the screen
+def clear_screen():
+    # Windows
     if name == 'nt':
-        _= system("cls")
-    
-    #mac ou linux
+        _ = system('cls')
+    # Mac or Linux
     else:
-        _= system("clear");
+        _ = system('clear')
 
+# Function to display the hangman
+def display_hangman(attempts):
+    # List of hangman stages
+    stages = [
+        """
+           --------
+           |      |
+           |      O
+           |     \\|/
+           |      |
+           |     / \\
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|/
+           |      |
+           |     / 
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|/
+           |      |
+           |      
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|
+           |      |
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |      |
+           |      |
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |    
+           |      
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      
+           |    
+           |      
+           |     
+           -
+        """
+    ]
+    return stages[attempts]
+
+# Main game function
 def game():
-
-    cleanScreen();
-    print("\nBem-vindo ao Jogo da Forca :)");
-    print("Adivinhe a palavra abaixo:\n");
-
-    #Lista de palavras
-    words = ['pitaya', 'groselha', 'abacate', 'morango', 'melao']
-
-    #Escolhendo uma palavra
-    word = random.choice(words);
-
-    #List Compreension: loop dentro de uma lista
-    letters = ['_' for letter in word];
-
-    #numero de chances
-    chance = 6;
+    clear_screen()
+    print("\nWelcome to the Hangman Game :)")
+    print("Guess the word below:\n")
     
-    #Lista para letras erradas
-    wrongLetters = [];
+    # List of words
+    words = ['morango', 'melancia', 'groselha', 'mirtilo', 'framboesa']
+    
+    # Choosing a random word
+    chosen_word = random.choice(words)
+    
+    # List of letters in the word
+    word_letters = [letter for letter in chosen_word]
+    
+    # Initial board
+    board = ["_"] * len(chosen_word)
+    
+    # Number of attempts
+    attempts = 6
+    
+    # List of wrong guesses
+    wrong_guesses = []
+    
+    # Game loop
+    while attempts > 0:
+        print(display_hangman(attempts))
+        print("\nWord: ", " ".join(board))
+        print("Wrong Guesses: ", " ".join(wrong_guesses))
+        
+        # Player's input
+        guess = input("\nEnter a letter: ").lower()
+        
+        # Validating guess
+        if guess in wrong_guesses or guess in board:
+            print("\nYou already guessed that letter. Try another.")
+            continue
+        
+        # Checking the guess
+        if guess in word_letters:
+            print("\nYou guessed a correct letter!")
+            for index in range(len(word_letters)):
+                if word_letters[index] == guess:
+                    board[index] = guess
+            if "_" not in board:
+                print("\nCongratulations! You won! The word was: {}".format(chosen_word))
+                break
+        else:
+            print("\nThat letter is not in the word.")
+            attempts -= 1
+            wrong_guesses.append(guess)
+    
+    # Losing condition
+    if "_" in board:
+        print("\nYou lost! The word was: {}.".format(chosen_word))
 
-    #Enquanto o número de chances maior que zero
-    while chance > 0:
-        print(" ".join(letters));
-        print("\nChances Restantes : ", chance);
-        print("Letras Erradas:", " ".join(wrongLetters))
-
-        tried = input("\nDigite uma letra: ").upper();
-
-        if tried in word:
-            index = 0
-            for letter in word:
-                if tried == letter:
-                    letters[index] = letter;
-                index +=1;
-
-        else: 
-            chance -= 1;
-            wrongLetters.append(tried);
-
-        if "_" not in letters:
-            print("\n Você venceu!, a palavra era: ", word);
-            break;
-
+# Main block
 if __name__ == "__main__":
     game()
+    print("\nWell done on completing the game! Keep learning Python!")
