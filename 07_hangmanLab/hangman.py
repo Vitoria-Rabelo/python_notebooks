adb67d23aabfe5bf82d5162fe3901fd787494540
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2024-12-27 22:31:31 -0300

    Classe Hangman e Metodos

--- 07_hangmanLab/hangman.py
+++ 07_hangmanLab/hangman.py
@@ -81,5 +81,61 @@ hangman_stages = [
     """
 ]
 
-def show(stage):
-    print(hangman_stages[stage])
+class Hangman:
+  #Construtor
+  def __init__(self,palavra):
+    self.palavra = palavra
+    #listas
+    self.letrasErradas = []
+    self.letrasEscolhidas = []
+  
+  #verificando letras escolhidas
+  def advinharLetra(self, letra):
+    if letra in self.palavra and letra not in self.letrasEscolhidas:
+      self.letrasEscolhidas.append(letra)
+    elif letra not in self.palavra and letra not in self.letrasEscolhidas:
+      self.letrasErradas.append(letra)
+    else: 
+      return False
+    return True
+
+#Verifica se o jogo terminou
+  def hangmanOver(self):
+    return self.hangmanWon() or (len(self.letrasErradas) == 6)
+
+#Verifica se o jogador venceu
+  def hangmanWon(self):
+    if '_' not in self.hidePalavra():
+      return True
+    return False
+  
+def printGameStatus(self):
+  print(board)
+def randPalavra():
+  palavras = ['professor', 'lousa', 'caderno', 'lapis', 'caneta', 'regua', 'giz']
+
+  palavra = random.choice(palavras)
+  return palavra 
+
+def main():
+  limpar_tela()
+
+  #Cria o objeto e relaciona randomicamente
+  game = Hangman(randPalavra())
+
+  #Enquanto o jogo não terminar:
+  while not game.hangmanOver():
+    game.printGameStatus()
+    user_input = input('\nDigite uma letra: ')
+    game.guess(user_input)
+
+  game.printGameStatus()
+  if game.hangmanWon():
+    print('Parabens voce venceu o jogo')
+  else:
+    print('Game Over!\n')
+    print('Você perdeu')
+
+#Executa o programa
+if __name__== "__main__":
+  main()
