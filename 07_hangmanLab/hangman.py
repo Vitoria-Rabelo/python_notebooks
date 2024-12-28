03ddfb3f608bfc241aa6f47548ff54e0055cfe3f
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2024-12-27 21:29:22 -0300

    Jogo da forca parte 2: Implementando método de limpar tela

---
+++ 07_hangmanLab/hangman.py
@@ -0,0 +1,85 @@
+# Jogo da forca parte 2 
+
+#funcao para limpar a tela a cada execução
+import random
+from os import system, name
+
+def limpar_tela():
+    #Windows
+    if name == 'nt':
+        _ =system ('cls')
+
+    #Mac ou Linux
+    else:
+      _ = system('clear')
+
+print("Hangman\n")
+
+hangman_stages = [
+    """
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+          |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     /    |
+          |
+    =========
+    """,
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     / \\  |
+          |
+    =========
+    """
+]
+
+def show(stage):
+    print(hangman_stages[stage])
